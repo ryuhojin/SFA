105c9721837157057beb1ec322e41d76f6dd8b67
류호진 <interarea@naver.com>
2023-03-28 21:37:54 +0900

    once 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const hi = () => {
-  return "hi";
+/**
+ * @name once
+ * @param func : Function
+ * @returns Function
+ * @description 단한번만 사용될 함수를 작성하기 위한 함수.
+ * ex) 더블클릭방지
+ */
+const once = (func: Function) => {
+  let ran: Boolean = false;
+  let result: Function;
+  return function (this: any) {
+    if (ran) return result;
+    result = func.apply(this, arguments);
+    ran = true;
+    return result;
+  };
 };
 
-export default hi;
+export default { once };
